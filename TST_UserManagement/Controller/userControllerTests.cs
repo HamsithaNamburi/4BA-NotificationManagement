eb87a5823e1a945cc4ae2287ebd3b99679850816
Madhuvani3305 <balanagumadhuvani@gmail.com>
2020-05-07 19:42:22 +0530

    tests

--- TST_UserManagement/Controller/userControllerTests.cs
+++ TST_UserManagement/Controller/userControllerTests.cs
@@ -1,19 +1,19 @@
-﻿using Moq;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using Ubiety.Dns.Core;
-using UserManagement;
+﻿//using Moq;
+//using NUnit.Framework;
+//using System;
+//using System.Collections.Generic;
+//using System.Net.Http;
+//using System.Text;
+//using Ubiety.Dns.Core;
+//using UserManagement;
 
-namespace UserManagementTestCases.Controller
-{
-    [TestFixture]
-    class userControllerTests
-    {
-        private UserController userController;
-        private Mock
+//namespace UserManagementTestCases.Controller
+//{
+//    [TestFixture]
+//    class userControllerTests
+//    {
+//        private UserController userController;
+//        private Mock
 
-    }
-}
+//    }
+//}
